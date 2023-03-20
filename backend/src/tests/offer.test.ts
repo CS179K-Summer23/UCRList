c0dcc37c928cbd064e5c6f1c1ff7ca89366ad644
cavelazquez8 <cavelazquez8@gmail.com>
2023-03-20 14:53:28 -0700

    rating tests

--- backend/src/tests/offer.test.ts
+++ backend/src/tests/offer.test.ts
@@ -1,4 +1,7 @@
 import { getOffer, createOffer } from '../controllers/offers';
+import { rating } from '../controllers/offers';
+import offerModel from '../models/offer';
+import * as OffersController from '../controllers/offers';
 import { MongoClient } from 'mongodb';
 //import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
@@ -18,6 +21,7 @@ describe('User Test', () => {
 
 	afterAll(async () => {
 		await connection.close();
+
 	});
 
 	it('Get Offer Test', async () => {
@@ -59,3 +63,84 @@ describe('User Test', () => {
 		expect(response.statusCode).toBe(200);
 	});
 });
+	describe('Rating Test', () => {
+		let connection;
+		const uri =
+		'mongodb+srv://gruiz031:U0WUKyxlFU1M0hgD@cluster0.cacviy0.mongodb.net/ucr_list?retryWrites=true&w=majority';
+	
+		beforeAll(async () => {
+		connection = await MongoClient.connect(uri);
+		});
+	
+		afterAll(async () => {
+		  await connection.close();
+
+	});
+  
+	it('Add new rating', async () => {
+  const request = httpMocks.createRequest({
+			method: 'PUT',
+			url: '/offers/rating',
+			body: {
+			star: 4,
+			comment: 'Good product',
+			offerId: '641200397bc49cb4936f9236',
+			postUsername: 'MockUser'
+		 	},
+			session: {
+		 		 userID: 'c@gmail.com',
+			},
+		  });
+	  
+		  const response = httpMocks.createResponse();
+		  const next = jest.fn();
+		  await rating(request, response, next);
+		  expect(response.statusCode).toBe(200);
+		}, 700000);
+	  
+		it('Update existing rating', async () => {
+		  const request = httpMocks.createRequest({
+			method: 'PUT',
+			url: '/offers/rating',
+			body: {
+			  star: 5,
+			  comment: 'Excellent product',
+			  offerId: '640d291b7353b7b93125a701',
+			  postUsername: 'MockUser'
+			},
+			session: {
+			  userID: 'c@gmail.com',
+			},
+		  });
+	  
+		  const response = httpMocks.createResponse();
+		  const next = jest.fn();
+		  await rating(request, response, next);
+		  expect(response.statusCode).toBe(200);
+		}, 700000);
+	  
+		it('Invalid offerId', async () => {
+		  const request = httpMocks.createRequest({
+			method: 'PUT',
+			url: '/offers/rating',
+			body: {
+			  star: 4,
+			  comment: 'Good product',
+			  offerId: 'invalid_offer_id',
+			  postUsername: 'MockUser'
+			},
+			session: {
+			  userID: 'c@gmail.com',
+			},
+		  });
+	  
+		  const response = httpMocks.createResponse();
+		  const next = jest.fn();
+		  await rating(request, response, next);
+		  expect(next).toHaveBeenCalledWith(expect.objectContaining({
+			message: 'Invalid offer id',
+			status: 400
+		  }));
+		});
+	  
+	 });
